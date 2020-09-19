f81a63002107a64f9807caa58a60f42447b6ed14
7max18 <maxkoppel0703@gmail.com>
2020-09-19 18:29:57 -0400

    Made Demo of character movement and hidden/false platform mechanics

---
+++ Asset Jam/Assets/Scripts/Magic Mirror/FollowMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, 0f);
+    }
+}
