8b9d70bc406c3dfd1b5e1eb0f9a77a2a56f71673
7max18 <maxkoppel0703@gmail.com>
2020-09-20 12:41:12 -0400

    Modified level made by Quinn-Hub

---
+++ Asset Jam/Assets/My Scripts/Character/ToggleLens.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLens : MonoBehaviour
+{
+    bool isEnabled = false;
+    GameObject spriteMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteMask = GameObject.Find("SpriteMask");
+        spriteMask.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            isEnabled = !isEnabled;
+            spriteMask.SetActive(isEnabled);
+        }
+
+    }
+}
